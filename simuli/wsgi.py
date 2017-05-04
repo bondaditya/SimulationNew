610ae25ad50e882e94fb32727a50d16d7ab19198
bondaditya <guptaadityaiitb@gmail.com>
2017-05-04 21:19:28 +0530

    first commit

--- simuli/wsgi.py
+++ simuli/wsgi.py
@@ -1,4 +1,4 @@
-"""
+ """
 WSGI config for simuli project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/bondaditya/simuli'
+
+if path not in sys.path:
+	sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simuli.settings")
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
